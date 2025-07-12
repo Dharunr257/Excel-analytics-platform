71902432a1e66e7afc558111880b4a6f7f636168
Dharun257 <dharunkumar257@gmail.com>
2025-07-12 17:16:28 +0530

    31st

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,9 @@ const __dirname = dirname(__filename);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
   );
 }
 
